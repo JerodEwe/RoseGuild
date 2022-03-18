e83fb166a5939765821536ee1b6686e46a2e380d
Jerod Ewert <jewert@starbucks.com>
2022-03-17 23:48:22 -0700

    added first test of basic structure

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,12 @@
+const assert = require('assert');
 const rose = require('../src/gilded_rose.js');
 describe("Test Gilded Rose", function() {
-rose.update_quality
-  it("should all have quality and sell_in properties", function() {
-    update_quality();
+
+  it("should all have the \'quality\' property", function() {
+    let items = rose.update_quality();
+    let count = items.reduce((previous, current, total)=>{
+      return total + (current.quality?1:0)
+    })
+    assert.equal(items.length, count);
   });
 });
